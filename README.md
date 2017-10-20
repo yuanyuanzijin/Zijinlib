9d296e109a4ac13f287a0df3249820da3c8b7e87
Louie <jinluyuan@vip.qq.com>
2017-10-20 22:23:29 +0800

    Add some instructions

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # python-zijinlib
-
 Zijinlib is a common python package written by me. Every project in this repo will import modules from this package.
 
-## Install
+## Description
+Breif introductions.
+
+### zj.file
+Some methods of processing the file.
+
+#### zj.file.sort_suffix(path, suffix)
+在给定path中选出指定后缀名的文件，返回这些文件的绝对路径的数组。
+
+#### zj.file.detect_damaged_pictures(filelist)
+检测给定的图片路径列表中是否有损坏的图片。
+
+#### zj.file.delete_from_list(dellist)
+删除给定文件路径列表中的文件。
+
+#### zj.file.search_two_suffixes(sourcelist, targetlist)
+在给定的两组不同后缀名的文件路径列表中选出没有成对后缀的文件，返回这些文件的绝对路径的数组。
+
+### zj.mail
+Some methods of sending emails via python.
+
+#### zj.mail.send_email(emailaddr, content, subject)
+向给定emailaddr发送邮件，返回发送结果。
+
+## Installation
 So before you use, please run the code below in the root directory.
 
 `pip install git+https://github.com/yuanyuanzijin/python-zijinlib`
@@ -13,7 +36,7 @@ or
 
 Since this project is in a period of rapid development, please confirm the current version of your package when using this project. We recommend that you run the above instructions to update your package when you update this repo.
 
-## Use
+## Usage
 The document will be written soon. Before it has been written, please refer the examples in the projects folder.
 
 The project folder contains several examlpes of using this python package. For specific instructions, see the various folders. Each folder is a script project.
--- zijinlib/file.py
+++ zijinlib/file.py
@@ -1,18 +1,18 @@
 import os
 import imghdr
 
-def search_two_suffixes(sourcelist, targetlist):
-    targetlist_new = []
+def sort_suffix(path, suffix):
+    filelist = []
+    for f in os.listdir(path):
+        if f.endswith(suffix):
+            filelist.append(path+f)
+    return filelist
+
+def detect_damaged_pictures(filelist):
     dellist = []
-    for t in targetlist:
-        target = t.split('.')[:-1]
-        targetlist_new.append(target)
-        
-    for s in sourcelist:
-        source = s.split('.')[:-1]
-        # 不存在对应的target文件则删除source文件
-        if source not in targetlist_new:
-            dellist.append(s)
+    for f in filelist:
+        if not imghdr.what(f):
+            dellist.append(f)
     return dellist
 
 def delete_from_list(dellist):
@@ -28,16 +28,16 @@ def delete_from_list(dellist):
             print(delpath+'删除失败')
     return delnumsuccess, delnumfail
 
-def sort_suffix(path, suffix):
-    filelist = []
-    for f in os.listdir(path):
-        if f.endswith(suffix):
-            filelist.append(path+f)
-    return filelist
-
-def detect_damaged_pictures(filelist):
+def search_two_suffixes(sourcelist, targetlist):
+    targetlist_new = []
     dellist = []
-    for f in filelist:
-        if not imghdr.what(f):
-            dellist.append(f)
+    for t in targetlist:
+        target = t.split('.')[:-1]
+        targetlist_new.append(target)
+        
+    for s in sourcelist:
+        source = s.split('.')[:-1]
+        # 不存在对应的target文件则删除source文件
+        if source not in targetlist_new:
+            dellist.append(s)
     return dellist
