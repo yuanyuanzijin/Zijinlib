2c8319b175d957e70ec47d730ef58e69c63f832f
Louie <jinluyuan@vip.qq.com>
2017-10-20 18:16:53 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+      name="zijinlib",
+      version="0.10",
+      description="A python package.",
+      author="yuanyuanzijin",
+      url="https://github.com/yuanyuanzijin",
+      license="LGPL",
+      packages= find_packages(),
+      scripts=["zijinlib"],
+      )
