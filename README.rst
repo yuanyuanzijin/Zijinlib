6b7d92a273ad441c119b77d38280f80a97a2e9db
Louie <jinluyuan@vip.qq.com>
2017-10-21 00:12:49 +0800

    upload to pypi

--- README.rst
+++ README.rst
@@ -1,7 +1,13 @@
-# python-zijinlib
+===============================================
+python-zijinlib
+===============================================
+
+
 Zijinlib is a common python package written by me. Every project in this repo will import modules from this package.
 
-## Description
+Description
+================
+
 Breif introductions.
 
 ### zj.file
@@ -25,7 +31,9 @@ Some methods of sending emails via python.
 #### zj.mail.send_email(emailaddr, content, subject)
 向给定emailaddr发送邮件，返回发送结果。
 
-## Installation
+Installation
+=======
+
 So before you use, please run the code below in the root directory.
 
 `pip install git+https://github.com/yuanyuanzijin/python-zijinlib`
@@ -36,8 +44,9 @@ or
 
 Since this project is in a period of rapid development, please confirm the current version of your package when using this project. We recommend that you run the above instructions to update your package when you update this repo.
 
-## Usage
+Usage
+=============
+
 The document will be written soon. Before it has been written, please refer the examples in the projects folder.
 
 The project folder contains several examlpes of using this python package. For specific instructions, see the various folders. Each folder is a script project.
-
--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
-      name="zijinlib",
-      version="0.11",
-      description="A python package.",
-      author="yuanyuanzijin",
-      url="https://github.com/yuanyuanzijin",
-      packages=["zijinlib"],
+    name='zijinlib',
+    version="0.12.1",
+    description=(
+        'A python package to process files and other things.'
+    ),
+    long_description=open('README.rst', 'rb').read().decode('utf-8'),
+    author='yuanyuanzijin',
+    author_email='yuanyuanzijin@gmail.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/yuanyuanzijin',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
 )
