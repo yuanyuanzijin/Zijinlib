aa63ce3e964abd54eef1e79c399174c73d8fac4d
Louie Jin <jinluyuan@vip.qq.com>
2017-10-21 16:57:39 +0800

    优化mail.py

--- zijinlib/mail.py
+++ zijinlib/mail.py
@@ -2,19 +2,21 @@ import smtplib
 import os
 from email.mime.text import MIMEText
 from email.header import Header
+import ConfigParser 
 
-def mail_init():
-    secret_path = os.path.join('excel_info_sending', "secret.txt")
-    with open(secret_path) as f:
-        content = f.readlines()
-        mail_host = content[0].strip()  #设置服务器
-        mail_user = content[1].strip()
-        mail_pass = content[2].strip()
-        sender = content[3].strip()
-    return mail_host, mail_user, mail_pass, sender
+def init(config_path):
+    c = ConfigParser.ConfigParser() 
+    with open(config_path) as f: 
+        c.readfp(f) 
+        mail_host = config.get('info', 'mail_host')  #设置服务器
+        mail_port = config.get('info', 'mail_port')
+        mail_user = config.get('info', 'mail_user')
+        mail_pass = config.get('info', 'mail_pass')
+        sender = config.get('info', 'sender')
+    return mail_host, mail_port, mail_user, mail_pass, sender
 
-def send_email(emailaddr, content, subject):
-    mail_host, mail_user, mail_pass, sender = mail_init()
+def send(config, emailaddr, content, subject):
+    mail_host, mail_port, mail_user, mail_pass, sender = config
     receivers = [emailaddr]  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
     mail_msg = content
     m = MIMEText(mail_msg, 'html', 'utf-8')
@@ -23,9 +25,9 @@ def send_email(emailaddr, content, subject):
     m['Subject'] = Header(subject, 'utf-8')
     try:
         smtpObj = smtplib.SMTP_SSL() 
-        smtpObj.connect(mail_host, 465)
-        smtpObj.login(mail_user,mail_pass)
-        smtpObj.sendmail(mail_user, receivers, m.as_string())
+        smtpObj.connect(mail_host, mail_port)
+        smtpObj.login(mail_user, mail_pass)
+        smtpObj.sendmail(sender, receivers, m.as_string())
         return True
     except smtplib.SMTPException:
         return False
