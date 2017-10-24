26c4e1b0b84dc87c60e9d8d23a8e9957df57eaed
Louie <jinluyuan@vip.qq.com>
2017-10-24 21:28:28 +0800

    增加qqrobot

--- projects/qq_robot/qqlib.py
+++ projects/qq_robot/qqlib.py
@@ -1,15 +1,9 @@
 import os
 
-members = ['小王', '小李']
-groups = ['群名', ]
-
 def send(members, content):
     for name in members:
         os.system('qq send buddy %s %s' % (name, content))
 
 def send_group(groups, content):
     for group in groups:
-        os.system('qq send group "%s" "%s"' % (group, content))
-
-send(members, '嘿嘿')
-send(groups, '大家好')
+        os.system('qq send group %s %s' % (group, content))
---
+++ projects/qq_robot/test1.py
@@ -0,0 +1,8 @@
+import os
+import qqlib as qq
+
+members = ['小王', '小李']
+groups = ['344247954', ]
+
+qq.send(members, '嘿嘿')
+qq.send_group(groups, '大家好！')
